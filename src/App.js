0528e80bf3885e122b6c412df8b35d895783f61d
Will Hackett <will@willhackett.com>
2017-02-24 14:31:29 +1000

    new bnuild

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Brand from './Brand';
+import Data from './Data';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Brand />
+        <Data />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Brand.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Brand.css';
+
+class Brand extends Component {
+  render() {
+    return (
+      <nav id="NAV_1">
+      	<figure id="FIGURE_2">
+      		 <a href="/" id="A_3">Svbtle</a>
+      	</figure>
+      	<h2 id="H2_4">
+      		<a href="//willhackett.com" id="A_5">Will Hackett</a>
+      	</h2>
+      </nav>
+    );
+  }
+}
+
+export default Brand;
