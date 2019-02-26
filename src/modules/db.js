b9ffe8c67ba32c603ad27b3a0fccc625748d4e78
Will Hackett <whackett@seek.com.au>
2019-02-26 18:21:39 +1100

    more fix

--- src/modules/db.js
+++ src/modules/db.js
@@ -2,13 +2,21 @@ import firebase from 'firebase/app';
 import rebase from 're-base';
 import 'firebase/database';
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyD7tM56qEhIv01OMXNJ83-PpTya-tOWeiM',
-  authDomain: 'will-hackett.firebaseapp.com',
-  databaseURL: 'https://will-hackett.firebaseio.com',
-  projectId: 'will-hackett',
-  storageBucket: 'will-hackett.appspot.com',
-  messagingSenderId: '567999273365'
-});
+// export default rebase.createClass(firebase.database());
 
-export default rebase.createClass(firebase.database());
+export default () => {
+  if (!window)
+    return {
+      bindToState: () => null
+    };
+  firebase.initializeApp({
+    apiKey: 'AIzaSyD7tM56qEhIv01OMXNJ83-PpTya-tOWeiM',
+    authDomain: 'will-hackett.firebaseapp.com',
+    databaseURL: 'https://will-hackett.firebaseio.com',
+    projectId: 'will-hackett',
+    storageBucket: 'will-hackett.appspot.com',
+    messagingSenderId: '567999273365'
+  });
+
+  return rebase.createClass(firebase.database());
+};
