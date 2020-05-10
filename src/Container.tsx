81e4826eb42c3dc77f48343478527ef14cb8c77d
Will Hackett <willhackett@me.com>
2020-05-10 18:54:16 +1000

    style: 💄 media queries

--- src/Container.tsx
+++ src/Container.tsx
@@ -5,7 +5,13 @@ const Container = styled.div`
   height: calc(100vh - 10rem);
   flex-direction: column;
   justify-content: space-between;
-  padding: 5rem 8rem;
+  padding: 5rem 4rem;
+  @media screen and (min-width: 600px) {
+    padding: 5rem 6rem;
+  }
+  @media screen and (min-width: 992px) {
+    padding: 5rem 8rem;
+  }
 `;
 
 export default Container;
